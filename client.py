9093798be650e8e8041fdf015e3adcb9642bb3ee
jenstem <jennifer.stemkowski@gmail.com>
2024-05-06 15:11:45 -0400

    Send message from server to client

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 HOST_NAME = socket.gethostname()
-PORT = 12345
+PORT = 8000
 
 s.connect((HOST_NAME, PORT))
+
+msg = s.recv(100)
+print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST_NAME = socket.gethostbyname()
-PORT = 12345
+HOST_NAME = socket.gethostbyname("127.0.0.1")
+PORT = 8000
 
 s.bind((HOST_NAME, PORT))
 s.listen(4)
 
 while True:
     c, addr = s.accept()
-    print('Got connection from', addr)
+    c.send(bytes("Hello, I am the server", "utf-8"))
+    print(addr)
